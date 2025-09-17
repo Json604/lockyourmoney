7c65416d06c6ba0e3dd74bfb7d6ec80682786441
Json604 <kartikey060105@gmail.com>
2025-09-17 18:10:01 +0530

    rate limitting implemented

--- backend/App.js
+++ backend/App.js
@@ -5,11 +5,13 @@ import userRouter from './routes/user.route.js';
 import connectToDB from './database/mongodb.js';
 import paymentRouter from './routes/payment.route.js';
 import errorMiddleware from './middlewares/error.middleware.js';
-
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
 app.use(express.json())
+
+app.use(arcjetMiddleware)
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
--- backend/config/env.js
+++ backend/config/env.js
@@ -9,4 +9,6 @@ export const{
     DB_URI,
     JWT_SECRET,
     JWT_EXPIRES_IN,
+    ARCJET_KEY,
+    ARCJET_ENV
 } = process.env;
